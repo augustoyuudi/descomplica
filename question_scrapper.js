8829366d99aa9ad9c20ba1802be0316266d908b6
augustoyuudi <augustoyuudi@gmail.com>
2023-09-01 14:11:09 -0300

    chore: move getQuestion function inside getAllQuestions

--- question_scrapper.js
+++ question_scrapper.js
@@ -1,28 +1,28 @@
-function getQuestion(questionCounter) {
-  return new Promise(resolve => {
-    setTimeout(() => {
-      const question = document.querySelector('.question__description__text').textContent;
-      const alternatives = document.querySelectorAll('.question__alternative');
-      let fullQuestion = `${questionCounter}. ${question}\n\n`;
-
-      const map = {
-        0: 'a',
-        1: 'b',
-        2: 'c',
-        3: 'd',
-        4: 'e',
-      }
-
-      alternatives.forEach((alternative, index) => {
-        fullQuestion += `${map[index]}) ${alternative.textContent}\n`;
-      });
-
-      resolve(fullQuestion);
-    }, 1000);
-  });
-}
-
 await (async function getAllQuestions() {
+  function getQuestion(questionCounter) {
+    return new Promise(resolve => {
+      setTimeout(() => {
+        const question = document.querySelector('.question__description__text').textContent;
+        const alternatives = document.querySelectorAll('.question__alternative');
+        let fullQuestion = `${questionCounter}. ${question}\n\n`;
+
+        const map = {
+          0: 'a',
+          1: 'b',
+          2: 'c',
+          3: 'd',
+          4: 'e',
+        }
+
+        alternatives.forEach((alternative, index) => {
+          fullQuestion += `${map[index]}) ${alternative.textContent}\n`;
+        });
+
+        resolve(fullQuestion);
+      }, 1000);
+    });
+  }
+
   const pages = document.querySelectorAll('.pagination__item');
 
   const allQuestions = [];
@@ -38,7 +38,7 @@ await (async function getAllQuestions() {
   await navigator.clipboard.writeText(allQuestions.join('\n\n'));
 })()
 
-(function scrapeOneQuestion() {
+(function getOneQuestion() {
   const question = document.querySelector('.question__description__text').textContent;
   const alternatives = document.querySelectorAll('.question__alternative');
   let fullQuestion = `1. ${question}\n\n`;
